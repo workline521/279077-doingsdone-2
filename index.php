ea7b5f7e5bc456ba1afd70f8ac98d8c7c42b245d
Evgenii Isupov <workline521@gmail.com>
2017-12-13 09:09:38 +0400

    Арифметика и работа со временем

--- index.php
+++ index.php
@@ -150,8 +150,9 @@ $days_until_deadline = floor(($task_deadline_ts - $current_ts) / 86400);
                         </td>
 
                         <td class="task__date">
-                            <!--выведите здесь дату выполнения задачи-->
+
                             <?= $date_deadline ?>
+
                         </td>
 
                         <td class="task__controls">
